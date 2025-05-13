1f477294f451d7302dbbcafe2b995b6166e8b808
varshnimurali <vmurali0@chicagobooth.edu>
2025-05-13 15:33:50 +0000

    save updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Allow server to be hosted on any URL
   config.hosts.clear
   # Allow better_errors to work in online IDE
-  config.web_console.allowed_ips = "0.0.0.0/0.0.0.0"
+  config.web_console.whitelisted_ips = "0.0.0.0/0.0.0.0"
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0.0.0.0"
   # Auto-connect to database when rails console opens
   console do
@@ -15,7 +16,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.enable_reloading = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -65,9 +66,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
-
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -75,14 +73,8 @@ Rails.application.configure do
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  config.action_view.annotate_rendered_view_with_filenames = true
+  # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
-
-  # Allow POST authenticity on Codespaces in dev
-  config.action_controller.forgery_protection_origin_check = false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "boards", :action => "index" })
+  devise_for :users
+
+  root to: "boards#index"
+
+  # get("/", { :controller => "boards", :action => "index" })
 
   # Routes for the Post resource:
 
   # CREATE
   post("/insert_post", { :controller => "posts", :action => "create" })
-          
+
   # READ
   get("/posts", { :controller => "posts", :action => "index" })
-  
+
   get("/posts/:path_id", { :controller => "posts", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
-  
+
   # DELETE
   get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
 
@@ -24,16 +28,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_board", { :controller => "boards", :action => "create" })
-          
+
   # READ
   get("/boards", { :controller => "boards", :action => "index" })
-  
+
   get("/boards/:path_id", { :controller => "boards", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_board/:path_id", { :controller => "boards", :action => "update" })
-  
+
   # DELETE
   get("/delete_board/:path_id", { :controller => "boards", :action => "destroy" })
 
